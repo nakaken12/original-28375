b17aa80e30f69cb3f90af74b812035ea5261e7c2
nakaken12 <sypk2105@ezweb.ne.jp>
2020-10-28 16:07:06 +0900

    ユーザー認証機能実装(devise)

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :reactions
+
+  validates :nickname, presence: true
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字で入力してください' } do
+    validates :password
+  end
+         
+end
