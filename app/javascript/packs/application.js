5e6d19bfd18dd5d49e4b15101631ed9bae207afd
nakaken12 <sypk2105@ezweb.ne.jp>
2020-11-07 22:55:03 +0900

    jqueryの記述箇所修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.jQuery = window.$ = require('jquery')
+require("../jquery_raty")
+
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../genre")
-
-window.jQuery = window.$ = require('jquery')
-require("../jquery_raty")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
